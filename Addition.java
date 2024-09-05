708591411c15b10452a7bbe663cc73721e2f4a5b
Neha0108 <112578569+Neha0108@users.noreply.github.com>
2024-09-05 09:53:06 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,17 +1,8 @@
-import java.util.Scanner;
-
 public class Addition {
     public static void main(String[] args) {
-        // Create a Scanner object to read input
-        Scanner scanner = new Scanner(System.in);
-
-        // Input first number
-        System.out.print("Enter first number: ");
-        int num1 = scanner.nextInt();
-
-        // Input second number
-        System.out.print("Enter second number: ");
-        int num2 = scanner.nextInt();
+        // Declare two numbers
+        int num1 = 10;
+        int num2 = 20;
 
         // Perform addition
         int sum = num1 + num2;
